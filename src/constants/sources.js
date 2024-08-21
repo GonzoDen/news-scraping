addf68c284b4eb9dca7c0559634821cd993ab146
gonzoden <deniz.nazarova.99@gmail.com>
2024-08-21 22:55:32 +0600

    set up upl scraping

--- src/constants/sources.js
+++ src/constants/sources.js
@@ -66,10 +66,35 @@ const NUZ_SOURCE = {
   descriptionSelector: "div > .td-module-meta-info > .td-excerpt",
 };
 
+const UPL_SOURCE = {
+  baseUrl: "https://upl.uz/",
+  searchQuery: "index.php?do=search&subaction=search&search_start=",
+  //searchSuffix: "&full_search=0&story=убита",
+  //searchSuffix: "&full_search=0&story=найдено+тело+женщины",
+  //searchSuffix: "&full_search=0&story=скончалась",
+  //searchSuffix: "&full_search=0&story=погибла",
+  //searchSuffix: "&full_search=0&story=труп+женщины",
+  //searchSuffix: "&full_search=0&story=смерти+избил",
+  //searchSuffix: "&full_search=0&story=скончалась+от+полученных+травм",
+  //searchSuffix: "&full_search=0&story=убийство+женщины",
+  //searchSuffix: "&full_search=0&story=изнасилование+женщины",
+  //searchSuffix: "&full_search=0&story=изнасилование+девушки",
+  searchSuffix: "&full_search=0&story=умерла",
+  numberOfPages: 1,
+  loadedElement: "#content-block", // The main container that ensures the page is fully loaded
+  dataContainer: ".sh-news", // Each news item is inside a <div> with class "sh-news"
+  linkSelector: ".sh-title a", // The <a> tag inside the <h2> tag that contains the link to the news article
+  titleSelector: ".sh-title a", // The same <a> tag inside the <h2> for extracting the title
+  dateSelector: ".side-date span:first-child", // The <span> element inside the .side-date div for the date
+  descriptionSelector: ".sh-text", // The <div> with class "sh-text" containing the description
+};
+
+
 module.exports = {
   PODROBNO_SOURCE,
   KUNUZ_SOURCE,
   GAZETA_SOURCE,
   UZNEWS_SOURCE,
   NUZ_SOURCE,
+  UPL_SOURCE,
 };
--- src/pageScraper.js
+++ src/pageScraper.js
@@ -5,7 +5,7 @@ const {
   PAGE_TIMEOUT,
 } = require("./constants/settings");
 const {
-  NUZ_SOURCE: source,
+  UPL_SOURCE: source,
 } = require("./constants/sources");
 
 const scraperObject = {
